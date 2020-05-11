8ee4c74d9aad42ed6e4f209367625c0d042f7af2
Porya <gohary.porya@gmail.com>
2020-05-11 19:27:05 +0430

    Add new functions for main scheduling.

--- src/proposedMothod.java
+++ src/proposedMothod.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Set;
+import java.util.*;
 
 /*******************************************************************************
  * Copyright © 2020 Pourya Gohari
@@ -35,22 +33,47 @@ public class proposedMothod {
     CPU cpu;
     String xml_name;
 
+    double overrun_percent;
+    int n_overrun;
+    Set<String> ov_tasks;
+
+
     boolean VERBOSE = false;
 
-    public proposedMothod(int deadline, int n_core, McDAG dag, String xml_name, boolean VERBOSE) {
+    public proposedMothod(int deadline, int n_core, McDAG dag, String xml_name, double overrun_percent, boolean VERBOSE) {
         this.deadline = deadline;
         this.n_core = n_core;
         this.dag = dag;
         this.xml_name = xml_name;
+        this.overrun_percent = overrun_percent;
         this.VERBOSE = VERBOSE;
     }
 
     public void start() throws Exception {
         //Sort Tasks from big to small by WCET LO
         sorted_tasks = sort_tasks(dag.getVertices().toArray(new Vertex[0]).clone());
+        ov_tasks = SelectOverrunTasks(dag.getNodes_HI().toArray(new Vertex[0]).clone());
         feasibility();
+        boolean finish = false;
+        while (!finish) {
+            boolean f = true;
+            try {
+                mainScheduling();
+            } catch (Exception e) {
+                if (VERBOSE) e.printStackTrace();
+                boolean x = drop_task();
+                if (!x) {
+                    System.out.println("Scheduling Problem!");
+                    System.exit(0);
+                }
+                f = false;
+            } finally {
+                if (f) finish = true;
+            }
+        }
     }
 
+    //Check Feasibility of System
     public void feasibility() throws Exception {
         String Task[];
         CPU cpu1 = new CPU(deadline, n_core, dag);
@@ -74,6 +97,12 @@ public class proposedMothod {
 
         cpu1.debug("TEST");
     }
+
+    //Main Scheduling of System
+    public void mainScheduling() throws Exception {
+
+    }
+
 
     //Get Tasks for every blocks
     public String[] get_tasks(boolean LO) {
@@ -130,5 +159,61 @@ public class proposedMothod {
             if (arr[i] == x) n++;
         }
         return n;
+    }
+
+    //Random Select Overrun tasks
+    public Set<String> SelectOverrunTasks(Vertex v[]) {
+        Set<String> ov_tasks = new HashSet<String>();
+        Set<String> temp = new HashSet<String>();
+
+        for (Vertex a : v) {
+            for (int i = 0; i < a.getReplica(); i++) {
+                temp.add(a.getName() + " CR" + i);
+            }
+        }
+
+        n_overrun = (int) (overrun_percent * temp.size());
+        String[] temp2 = temp.stream().toArray(String[]::new).clone();
+
+        //Select Tasks
+        Random ov = new Random();
+        int o;
+        for (int i = 0; i < n_overrun; i++) {
+            do {
+                o = ov.nextInt(temp2.length);
+            } while (ov_tasks.contains(temp2[o]));
+            ov_tasks.add(temp2[o]);
+        }
+        return ov_tasks;
+    }
+
+    //Drop LO Tasks That cannot schedule in offline phase
+    public boolean drop_task() {
+        for (int i = sorted_tasks.length - 1; i >= 0; i--) {
+            if (!sorted_tasks[i].isHighCr() && sorted_tasks[i].isRun()) {
+                sorted_tasks[i].setRun(false);
+                if (VERBOSE) System.out.println("■■■  DROP TASK " + sorted_tasks[i].getName());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // QoS Calculator
+    public double QoS() {
+        double QoS = 0;
+        for (int i = 0; i < sorted_tasks.length; i++) {
+            if (!sorted_tasks[i].isHighCr() && sorted_tasks[i].isRun()) QoS++;
+        }
+        QoS = QoS / (sorted_tasks.length - dag.getNodes_HI().size());
+        return QoS;
+    }
+
+    //Reset condition of scheduled tasks
+    public void reset_schedule() {
+        for (Vertex a : sorted_tasks) {
+            a.setDone(false);
+            a.setScheduled(0);
+        }
     }
 }
